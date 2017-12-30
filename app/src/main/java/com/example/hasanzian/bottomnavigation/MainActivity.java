9c2a6e2c432b83c1e51b08f0e590cd15be3ed9fd
hasanmohdkhan <hasanzian@gmail.com>
2017-12-30 14:41:51 +0530

    modify MainActivity.java

--- app/src/main/java/com/example/hasanzian/bottomnavigation/MainActivity.java
+++ app/src/main/java/com/example/hasanzian/bottomnavigation/MainActivity.java
@@ -1,20 +1,71 @@
 package com.example.hasanzian.bottomnavigation;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActionBar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
 
+       //toolbar
+        toolbar = getSupportActionBar();
 
+        //linking Navigation
+        BottomNavigationView navigation = (BottomNavigationView)findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+        toolbar.setTitle("Shop");
 
 
 
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment fragment;
+                    switch (item.getItemId()){
+
+                        case R.id.navigation_shop:
+                            toolbar.setTitle("Shop");
+                            return true;
+
+                        case R.id.navigation_gift:
+                            toolbar.setTitle("Gifts");
+                            return true;
+
+                        case R.id.navigation_cart:
+                            toolbar.setTitle("Cart");
+                            return true;
+
+                        case R.id.navigation_profile:
+                            toolbar.setTitle("Profile");
+                            return true;
+
+                    }
+
+
+
+
+
+
+
+                    return false;
+                }
+            };
+
+
+
+
+
 }
